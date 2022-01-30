95e97e92dccdb7d4df8320150716ee6acfce053c
Samer <samerkhallouf7@gmail.com>
2022-01-30 22:53:29 +0200

    finished

--- src/My_JDBC.java
+++ src/My_JDBC.java
@@ -1,19 +1,36 @@
 import java.sql.*;
-
 public class My_JDBC {
     public static void main(String[] args) {
         try{
             //1-Connect to a DB
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","Admin","Admin");
-            //2- Write statements
-            Statement firststmt = myConn.createStatement();
-
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
+            //2- Write statements to insert into table
+//            Statement firststmt = myConn.createStatement();
+//            String query = "insert into students(idStudents,last_name, first_name,notes)"+"values(?,?,?,?)";
+//
+//            PreparedStatement preparedStmt = myConn.prepareStatement(query);
+//            preparedStmt.setInt(1,1);
+//            preparedStmt.setString(2,"Khallouf");
+//            preparedStmt.setString(3,"Samer");
+//            preparedStmt.setInt(4,10);
+//
+//            PreparedStatement preparedStmt2 = myConn.prepareStatement(query);
+//            preparedStmt2.setInt(1,2);
+//            preparedStmt2.setString(2,"Khallouf");
+//            preparedStmt2.setString(3,"Elias");
+//            preparedStmt2.setInt(4,20);
             //3- Execute statements
-            ResultSet results = firststmt.executeQuery("select * from students");
-            //4- Show the result set
-            while (results.next()){
-                System.out.println(results.getString("last_name")+ " , "+ results.getString("first_name"));
+//            preparedStmt.execute();
+//            preparedStmt2.execute();
+
+            //4- Write statements to get results from table
+            Statement myStmt = myConn.createStatement();
+            ResultSet results = myStmt.executeQuery("select * from students");
+            //5- Show the result of the query
+            while(results.next()){
+                System.out.println(results.getInt("idStudents")+" "+results.getString("last_name")+ " "+results.getString("first_name")+" : "+results.getInt("notes"));
             }
+            myConn.close();
         }catch (Exception exc){
             exc.printStackTrace();
         }
